16420dfa5bd5b335ec08d763b29e425ece28c87d
andosty <35182275+andosty@users.noreply.github.com>
2025-07-10 16:14:30 +0000

    Delete main directory

--- main/Rscripts/Data Download/getQuestionList.R
+++
@@ -1,6 +0,0 @@
-questlist <- suso_getQuestDetails(workspace =Sys.getenv("new_wp") ) %>%  #currentWorkSpace
-  filter( 
-    Variable == "ibes_ii"
-  ) %>%
-  mutate(    Queue_job_id = NA_integer_  ) %>%
-  filter(Version == currentDataVersion)
